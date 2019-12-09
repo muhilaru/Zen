8054efda89c9aac8023dc262ea54a7b66ce63cc4
muhila2 <muhila2@illinois.edu>
2019-12-09 16:37:38 -0600

    Completed Project

--- app/src/main/java/com/example/zenv2/MainActivity.java
+++ app/src/main/java/com/example/zenv2/MainActivity.java
@@ -5,15 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.ramotion.foldingcell.FoldingCell;
 import com.varunest.sparkbutton.SparkButton;
 import com.varunest.sparkbutton.SparkEventListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import ticker.views.com.ticker.widgets.circular.timer.callbacks.CircularViewCallback;
 import ticker.views.com.ticker.widgets.circular.timer.view.CircularView;
@@ -34,24 +44,20 @@ public class MainActivity extends AppCompatActivity {
         a = savedInstanceState;
         playMusic();
 
+        //https://github.com/varunest/SparkButton
         final SparkButton start = findViewById(R.id.spark_button);
         start.setEventListener(new SparkEventListener(){
             @Override
             public void onEvent(ImageView button, boolean buttonState) {
                 if (buttonState) {
-                    // Button is active
                     new CountDownTimer(820, 1000) {
                         public void onFinish() {
-                            // When timer is finished
-                            // Execute your code here
                             enterNameMenu();
                         }
                         public void onTick(long millisUntilFinished) {
-                            // millisUntilFinished    The amount of time until finished.
                         }
                     }.start();
                 } else {
-                    // Button is inactive
                 }
             }
             @Override
@@ -73,19 +79,14 @@ public class MainActivity extends AppCompatActivity {
                     if (!nameGetter.getText().toString().equals("")) {
                         name = nameGetter.getText().toString();
                     }
-                    // Button is active
                     new CountDownTimer(820, 1000) {
                         public void onFinish() {
-                            // When timer is finished
-                            // Execute your code here
                             enterTabbedMenu();
                         }
                         public void onTick(long millisUntilFinished) {
-                            // millisUntilFinished    The amount of time until finished.
                         }
                     }.start();
                 } else {
-                    // Button is inactive
                 }
             }
             @Override
@@ -102,6 +103,7 @@ public class MainActivity extends AppCompatActivity {
         TextView tabTitle = findViewById(R.id.tabTitle);
         tabTitle.setText(name + ", open your mind");
 
+        //https://github.com/Ramotion/folding-cell-android
         final FoldingCell fc = (FoldingCell) findViewById(R.id.folding_cell);
         fc.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -133,19 +135,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onEvent(ImageView button, boolean buttonState) {
                 if (buttonState) {
-                    // Button is active
                     new CountDownTimer(820, 1000) {
                         public void onFinish() {
-                            // When timer is finished
-                            // Execute your code here
                             enterMenuBeforeTimer();
                         }
                         public void onTick(long millisUntilFinished) {
-                            // millisUntilFinished    The amount of time until finished.
                         }
                     }.start();
                 } else {
-                    // Button is inactive
                 }
             }
             @Override
@@ -164,19 +161,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onEvent(ImageView button, boolean buttonState) {
                 if (buttonState) {
-                    // Button is active
                     new CountDownTimer(820, 1000) {
                         public void onFinish() {
-                            // When timer is finished
-                            // Execute your code here
                             enterTimer();
                         }
                         public void onTick(long millisUntilFinished) {
-                            // millisUntilFinished    The amount of time until finished.
                         }
                     }.start();
                 } else {
-                    // Button is inactive
                 }
             }
             @Override
@@ -192,6 +184,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         final SparkButton donzo = findViewById(R.id.spark_button5);
         donzo.setVisibility(View.GONE);
+        //https://github.com/rehmanmuradali/ticker
         final CircularView circularViewWithTimer = findViewById(R.id.timer);
         circularViewWithTimer.startTimer();
         CircularView.OptionsBuilder builderWithTimer =
@@ -202,7 +195,6 @@ public class MainActivity extends AppCompatActivity {
                             @Override
                             public void onTimerFinish() {
                                 donzo.setVisibility(View.VISIBLE);
-                                // Will be called if times up of countdown timer
                                 stopMusic2();
                                 playMusic3();
                                 Toast.makeText(MainActivity.this, "Congratulations: Meditation Finished ", Toast.LENGTH_SHORT).show();
@@ -210,8 +202,6 @@ public class MainActivity extends AppCompatActivity {
 
                             @Override
                             public void onTimerCancelled() {
-
-                                // Will be called if stopTimer is called
                                 Toast.makeText(MainActivity.this, "CircularCallback: Timer Cancelled ", Toast.LENGTH_SHORT).show();
                             }
                         });
@@ -221,31 +211,23 @@ public class MainActivity extends AppCompatActivity {
         playMusic2();
         new CountDownTimer(1200, 1000) {
             public void onFinish() {
-                // When timer is finished
-                // Execute your code here
                 circularViewWithTimer.setVisibility(View.VISIBLE);
             }
             public void onTick(long millisUntilFinished) {
-                // millisUntilFinished    The amount of time until finished.
             }
         }.start();
         donzo.setEventListener(new SparkEventListener(){
             @Override
             public void onEvent(ImageView button, boolean buttonState) {
                 if (buttonState) {
-                    // Button is active
                     new CountDownTimer(820, 1000) {
                         public void onFinish() {
-                            // When timer is finished
-                            // Execute your code here
                             finish();
                         }
                         public void onTick(long millisUntilFinished) {
-                            // millisUntilFinished    The amount of time until finished.
                         }
                     }.start();
                 } else {
-                    // Button is inactive
                 }
             }
             @Override
@@ -258,6 +240,52 @@ public class MainActivity extends AppCompatActivity {
     }
     public void finish() {
         setContentView(R.layout.finishedmeditation);
+        final TextView quoteHolder = findViewById(R.id.quote);
+
+
+        //https://github.com/lukePeavey/quotable
+        String URL = "https://api.quotable.io/random";
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        JsonObjectRequest objectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                URL,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.e("rest response", response.toString());
+                        try {
+                            String quote = response.get("content").toString();
+                            String author = response.get("author").toString();
+                            quoteHolder.setText(quote + " ~ " + author);
+                        } catch (JSONException e) {
+                            Log.e("MYAPP", "unexpected JSON exception", e);
+                            // Do something to recover ... or kill the app.
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("rest response", error.toString());
+                    }
+                }
+        );
+
+        requestQueue.add(objectRequest);
+
+
+
+
+
+
+
+
+
+
+
         SparkButton six = findViewById(R.id.spark_button6);
         six.setEventListener(new SparkEventListener(){
             @Override
@@ -290,7 +318,7 @@ public class MainActivity extends AppCompatActivity {
     public void restart() {
         setContentView(R.layout.main_menu);
         playMusic();
-        name = "";
+        name = "You";
         final SparkButton start = findViewById(R.id.spark_button);
         start.setEventListener(new SparkEventListener(){
             @Override
